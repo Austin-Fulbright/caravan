d7138a5e38f77f8595586084a70eafd937f6e55e
Austin-Fulbright <austinfulbright@gmail.com>
2025-02-21 13:10:45 -0500

    added classes for jade.ts

--- packages/caravan-wallets/src/jade.ts
+++ packages/caravan-wallets/src/jade.ts
@@ -1,6 +1,6 @@
 import { DirectKeystoreInteraction, PENDING, ACTIVE, INFO } from './interaction'; // or your actual base class location
 import { JadeAPI } from 'jade-hw-api';
-
+import { BitcoinNetwork, ExtendedPublicKey } from "@caravan/bitcoin";
 
 export const JADE = "jade";
 
@@ -46,8 +46,28 @@ export class JadeInteraction extends DirectKeystoreInteraction {
    */
   async withDevice<T>(f: (jadeApi: JadeAPI) => Promise<T>): Promise<T> {
     try {
+      // Connect to the device.
       await this.jadeApi.connect();
-      //add code to unlock the device using authUser
+  
+      const httpRequestFn = async (params: any): Promise<{ body: any }> => {
+        const url = params.urls[0];
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(params.data),
+        });
+        if (!response.ok) {
+          throw new Error('HTTP request failed in authUser');
+        }
+        const body = await response.json();
+        return { body };
+      };
+  
+      const unlockResult = await this.jadeApi.authUser("mainnet", httpRequestFn);
+      if (unlockResult !== true) {
+        throw new Error("Failed to unlock Jade device");
+      }
+  
       try {
         return await f(this.jadeApi);
       } finally {
@@ -57,6 +77,7 @@ export class JadeInteraction extends DirectKeystoreInteraction {
       throw new Error("Error interacting with Jade device: ", err.message);
     }
   }
+  
 
   // Dummy run method to satisfy abstract interface requirements.
   async run(): Promise<any> {
@@ -90,3 +111,91 @@ export class JadeGetMetadata extends JadeInteraction {
     });
   }
 }
+export class JadeExportPublicKey extends JadeInteraction {
+
+  network: BitcoinNetwork;
+  
+  bip32Path: string;
+
+  includeXFP: boolean
+
+  constructor({ network, bip32Path, includeXFP }: {
+    network: BitcoinNetwork;
+    bip32Path: string;
+    includeXFP: boolean;
+  }) {
+    super();
+    this.network = network;
+    this.bip32Path = bip32Path;
+    this.includeXFP = includeXFP;
+  }
+  //make sure network comes out right
+
+  messages() {
+    return super.messages();
+  }
+
+  async run() {
+    return await this.withDevice(async (jadeApi)  => {
+      //need to convert the bip32Path into an integer array
+      const xpub = await jadeApi.getXpub(this.network, this.bip32Path);
+      const publicKey = ExtendedPublicKey.fromBase58(xpub).pubKey;
+      if (this.includeXFP) {
+        const rootFingerprint = await jadeApi.rootFingerprint();
+        return { publicKey, rootFingerprint };
+      }
+      return publicKey;
+
+    });
+  }
+}
+export class JadeExportExtendedPublicKey extends JadeInteraction {
+
+  network: BitcoinNetwork;
+  
+  bip32Path: string;
+
+  includeXFP: boolean
+
+  constructor({ network, bip32Path, includeXFP }: {
+    network: BitcoinNetwork;
+    bip32Path: string;
+    includeXFP: boolean;
+  }) {
+    super();
+    this.network = network;
+    this.bip32Path = bip32Path;
+    this.includeXFP = includeXFP;
+  }
+  //make sure network comes out right
+
+  messages() {
+    return super.messages();
+  }
+
+  async run() {
+    return await this.withDevice(async (jadeApi)  => {
+      //need to convert the bip32Path into an integer array
+      const xpub = await jadeApi.getXpub(this.network, this.bip32Path);
+      if (this.includeXFP) {
+        const rootFingerprint = await jadeApi.rootFingerprint();
+        return { xpub, rootFingerprint };
+      }
+      return xpub;
+
+    });
+  }
+
+}
+export class JadeRegisterWalletPolicy extends JadeInteraction {
+
+}
+
+export class JadeConfirmMultisigAddress extends JadeInteraction{
+
+}
+export class JadeSignMultisigTransaction extends JadeInteraction{
+
+}
+
+
